1069b8ce407ce5f2f7d439eb95bed6839e7abb4d
frank <wzuo@actiontec.com>
2015-11-16 16:45:40 -0800

    Update APP_ID for storebbs

--- src/android/src/WechatShare.java
+++ src/android/src/WechatShare.java
@@ -55,7 +55,7 @@ public class WechatShare extends CordovaPlugin {
         @Override
         public void run() {
           IWXAPI api;
-          String APP_ID = "wxb8587d398599a602";
+          String APP_ID = "wx2600c1664d3083bd";
           api = WXAPIFactory.createWXAPI(context, APP_ID, false);
           if (!api.isWXAppInstalled()) {
 
